902dda256eb7cba6472fc235442d54ef4ef55dcb
Maciej Przybylak <maciej.przybylak@allegro.pl>
2021-05-16 22:38:03 +0200

    java 11 heroku

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 group = "com.mackokodzi"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -28,7 +28,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "8"
+		jvmTarget = "11"
 	}
 }
 
